ffff2f2fc4600b5980b9c1b16b9af79592ad9c85
Lera <tyastovaleriya@gmail.com>
2023-11-14 22:35:45 +0500

    .

--- dbController.py
+++ dbController.py
@@ -1,16 +1,21 @@
 import sqlite3
 
-# подключение к базе данных
 connect = sqlite3.connect("db.db")
 cursor = connect.cursor()
 
-# создание таблиц базы данных и связей
 cursor.execute('''
-CREATE TABLE IF NOT EXISTS  "users" ("id"	Integer not null, "login" Text not null, "password"	Text not null, PRIMARY KEY("id" AUTOINCREMENT));
+CREATE TABLE IF NOT EXISTS  "users" (
+    "id"	Integer not null, 
+    "login" Text not null, 
+    "password"	Text not null, 
+    PRIMARY KEY("id" AUTOINCREMENT));
 ''')
 
 cursor.execute('''
-CREATE TABLE IF NOT EXISTS  "type_links" ("id"	Integer not null, "name" Text not null, PRIMARY KEY("id" AUTOINCREMENT));
+CREATE TABLE IF NOT EXISTS  "type_links" (
+    "id"	Integer not null, 
+    "name" Text not null, 
+    PRIMARY KEY("id" AUTOINCREMENT));
 ''')
 
 cursor.execute('''
@@ -34,6 +39,7 @@ def setTypes(arr):
         cursor.execute('''INSERT INTO type_links('name') VALUES(?);''', (i,))
         print(i)
     connect.commit()
+    connect.close()
 
 def getTypes():
     connect = sqlite3.connect("db.db")
@@ -52,14 +58,37 @@ def getPsev(psev):
 def getTypebyLink(link):
     connect = sqlite3.connect("db.db")
     cursor = connect.cursor()
-    return cursor.execute('SELECT type_id FROM links WHERE link = ?',(link,)).fetchone()
+    return cursor.execute('SELECT type_id FROM links WHERE  short_link = ?',(link,)).fetchone()
 
+def getLinksByUser(us_id):
+    connect = sqlite3.connect("db.db")
+    cursor = connect.cursor()
+    return cursor.execute('SELECT link, count, type_links.name as type, short_link as short, links.id, type_id FROM links INNER JOIN type_links ON type_id = type_links.id WHERE user_id = ?',(us_id,)).fetchall()
+
+def getUserbyLink(link):
+    connect = sqlite3.connect("db.db")
+    cursor = connect.cursor()
+    return cursor.execute('SELECT user_id FROM links WHERE short_link = ?',(link,)).fetchone()
 def updateCounfLink(link):
     connect = sqlite3.connect("db.db")
     cursor = connect.cursor()
-    cursor.execute("UPDATE links SET count = count+1 WHERE link = ?", (link, ))
+    cursor.execute("UPDATE links SET count = count+1 WHERE short_link = ?", (link, ))
     connect.commit()
+    connect.close()
 
+def editTypefLink(type_id, link_id):
+    connect = sqlite3.connect("db.db")
+    cursor = connect.cursor()
+    cursor.execute("UPDATE links SET type_id = ? WHERE id = ?", (type_id, link_id, ))
+    connect.commit()
+    connect.close()
+
+def editPsevfLink(psev, link_id):
+    connect = sqlite3.connect("db.db")
+    cursor = connect.cursor()
+    cursor.execute("UPDATE links SET short_link = ? WHERE id = ?", (psev, link_id, ))
+    connect.commit()
+    connect.close()
 def getPass(login, passw):
     connect = sqlite3.connect("db.db")
     cursor = connect.cursor()
@@ -70,19 +99,28 @@ def insertLink(link, us_id, type_id, short_l):
     cursor = connect.cursor()
     cursor.execute('''INSERT INTO links(link, user_id, type_id, short_link, count) VALUES(?,?,?,?, 0);''', (link, us_id, type_id, short_l,))
     connect.commit()
+    connect.close()
 
 def insertLinkNotAuth(link,type_id, short_l):
     connect = sqlite3.connect("db.db")
     cursor = connect.cursor()
     cursor.execute('''INSERT INTO links(link, user_id, type_id, short_link, count) VALUES(?, NULL, ?, ?, 0);''', (link, type_id, short_l,))
     connect.commit()
+    connect.close()
 
 def insertUser(login, password):
     connect = sqlite3.connect("db.db")
     cursor = connect.cursor()
     cursor.execute('''INSERT INTO users(id, login, password) VALUES(NULL, ?,?);''', (login, password,))
     connect.commit()
+    connect.close()
 
-# types = ["Публичная", "Общего доступа", "Приватная"]
-# setTypes(types)
+def deleteLink(link_id):
+    connect = sqlite3.connect('db.db')
+    cursor = connect.cursor()
+    cursor.execute('''DELETE FROM 'links' WHERE id = ?''', (link_id,))
+    connect.commit()
+    connect.close()
+
 connect.commit()
+connect.close()
